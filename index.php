811eb80767052c80a485d012b41b5acb28d837bb
aidyn47 <75780969+aidyn47@users.noreply.github.com>
2021-04-21 10:33:20 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+
+        </title>
+            <style> body{background:cadetblue}
+                
+            </style>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        
+    </body>
+</html>
